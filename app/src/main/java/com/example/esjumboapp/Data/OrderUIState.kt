fa7a6b6ac1fa06b9ca0b3fbc5191dd8da4e55ef3
DzakyNaufal <dzakynmulna@gmail.com>
2023-11-09 10:13:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/esjumboapp/Data/OrderUIState.kt
@@ -0,0 +1,7 @@
+package com.example.esjumboapp.Data
+
+data class OrderUIState (
+    val jumlah: Int = 0,
+    val rasa: String = "",
+    val harga: String = "",
+)
---
+++ app/src/main/java/com/example/esjumboapp/OrderViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.esjumboapp
+
+import androidx.lifecycle.ViewModel
+import com.example.esjumboapp.Data.OrderUIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import java.text.NumberFormat
+
+private const val HARGA_PER_CUP = 5000
+
+class OrderViewModel : ViewModel() {
+    private val _stateui = MutableStateFlow(OrderUIState())
+    val stateUI: StateFlow<OrderUIState> = _stateui.asStateFlow()
+
+    fun setJumlah(jmlEsTeler:Int){
+        _stateui.update { stateSaatIni ->
+            stateSaatIni.copy(
+                jumlah = jmlEsTeler,
+                harga = hitungHarga(jumlah = jmlEsTeler)
+            )
+        }
+    }
+
+    fun setRasa(rasaPilihan: String) {
+        _stateui.update { stateSaatIni ->
+            stateSaatIni.copy(rasa = rasaPilihan) }
+    }
+
+    fun resetOrder(){
+        _stateui.value = OrderUIState()
+    }
+
+    private fun hitungHarga(
+        jumlah: Int = _stateui.value.jumlah,
+    ): String {
+        val kalkulasHarga = jumlah * HARGA_PER_CUP
+
+        return NumberFormat.getNumberInstance().format(kalkulasHarga)
+    }
+}
